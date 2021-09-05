b8d0f5e519f4f4d918af80aa9e7ed795beaa2c41
Andres Carrero <andres_car098@hotmail.com>
2021-09-05 17:04:15 -0500

    Contando y buscando tareas

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./TodoCounter.css";
 
-function TodoCounter() {
-  return <h2 className="TodoCounter">Completadas 2 de 3 tareas.</h2>;
+function TodoCounter({ total, completed }) {
+  return (
+    <h2 className="TodoCounter">
+      Completadas {completed} de {total} tareas.
+    </h2>
+  );
 }
 
 export { TodoCounter };
