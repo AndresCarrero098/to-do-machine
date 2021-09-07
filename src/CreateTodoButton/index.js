91cbe2ea0d71764243918e1e6a37175c5bd7a5d8
Andres Carrero <andres_car098@hotmail.com>
2021-09-07 14:34:22 -0500

    Manejando Portals

--- src/CreateTodoButton/index.js
+++ src/CreateTodoButton/index.js
@@ -2,15 +2,12 @@ import React from "react";
 import "./CreateTodoButton.css";
 
 function CreateTodoButton(props) {
-  const onClickButton = (msg) => {
-    console.log(msg);
+  const onClickButton = () => {
+    props.setOpenModal(true);
   };
-  
+
   return (
-    <button
-      className="CreateTodoButton"
-      onClick={() => onClickButton("Nice click!")}
-    >
+    <button className="CreateTodoButton" onClick={onClickButton}>
       +
     </button>
   );
